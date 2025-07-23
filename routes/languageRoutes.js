c38b72b9b8af1052f59782edad85cc384b6e10e6
sherazaleem <107539547+sherazaleem@users.noreply.github.com>
2025-07-23 22:57:25 +0500

    update admins login and register

--- routes/languageRoutes.js
+++ routes/languageRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const languageController = require('../controllers/languageController');
 const authMiddleware = require('../middleware/auth');
+const isAdmin = require('../middleware/isAdmin');
 
 router.use(authMiddleware);
+router.use(isAdmin); // restrict all routes in this file to admin only
 
 router.post('/', languageController.create);
 router.get('/', languageController.getAll);
