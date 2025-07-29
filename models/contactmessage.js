5e7a63fdaaeed808a60f560d3b726a290852724e
sherazaleem <107539547+sherazaleem@users.noreply.github.com>
2025-07-29 11:02:49 +0500

    udapdate

--- models/contactmessage.js
+++ models/contactmessage.js
@@ -1,10 +1,18 @@
-'use strict';
+// models/contactmessage.js
 module.exports = (sequelize, DataTypes) => {
-  const ContactMessage = sequelize.define('ContactMessage', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    subject: DataTypes.STRING,
-    message: DataTypes.TEXT,
-  });
+  const ContactMessage = sequelize.define(
+    'ContactMessage',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      subject: DataTypes.STRING,
+      message: DataTypes.TEXT,
+    },
+    {
+      tableName: 'contactmessages', // 👈 use exact table name here
+      timestamps: true,             // 👈 keep this if you're using createdAt/updatedAt
+    }
+  );
+
   return ContactMessage;
 };
--- models/follow.js
+++ models/follow.js
@@ -1,8 +1,17 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  const Follow = sequelize.define('Follow', {
-    followerId: DataTypes.INTEGER,
-    followingId: DataTypes.INTEGER,
-  });
+  const Follow = sequelize.define(
+    'Follow',
+    {
+      followerId: DataTypes.INTEGER,
+      followingId: DataTypes.INTEGER,
+    },
+    {
+      tableName: 'follows',     // 👈 force Sequelize to use lowercase table name
+      timestamps: true,        // 👈 optional, remove if not using createdAt/updatedAt
+    }
+  );
+
   return Follow;
 };
