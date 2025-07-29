e304c4943b1fa3b25978133d65f779a92dc1b714
sherazaleem <107539547+sherazaleem@users.noreply.github.com>
2025-07-29 11:02:49 +0500

    udapdate

--- models/media.js
+++ models/media.js
@@ -1,14 +1,23 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  const Media = sequelize.define('Media', {
-    userId: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    fileUrl: DataTypes.STRING,
-    type: DataTypes.STRING,
-    visibility: DataTypes.STRING,
-    likes: DataTypes.INTEGER,
-    shares: DataTypes.INTEGER,
-  });
+  const Media = sequelize.define(
+    'Media',
+    {
+      userId: DataTypes.INTEGER,
+      title: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      fileUrl: DataTypes.STRING,
+      type: DataTypes.STRING,
+      visibility: DataTypes.STRING,
+      likes: DataTypes.INTEGER,
+      shares: DataTypes.INTEGER,
+    },
+    {
+      tableName: 'media',     // 👈 force table name to lowercase
+      timestamps: true,       // 👈 keep if you're using createdAt/updatedAt
+    }
+  );
+
   return Media;
 };
--- models/talentcategory.js
+++ models/talentcategory.js
@@ -1,19 +1,27 @@
-// models/talentcategory.js
 'use strict';
 const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class TalentCategory extends Model {
     static associate(models) {
+      // Define association here
       TalentCategory.hasMany(models.Talent, {
         foreignKey: 'categoryId'
       });
     }
   }
-  TalentCategory.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'TalentCategory',
-  });
+
+  TalentCategory.init(
+    {
+      name: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'TalentCategory',
+      tableName: 'talentcategories', // 👈 Use exact lowercase table name
+      timestamps: true             // 👈 Optional: include if you want createdAt/updatedAt
+    }
+  );
+
   return TalentCategory;
 };
