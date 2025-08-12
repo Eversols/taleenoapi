5b9fbf524973b6da24bad0bfc51fdc5d34b4c2ce
sherazaleem <107539547+sherazaleem@users.noreply.github.com>
2025-08-12 21:56:24 +0500

    pagination on nootes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -6,6 +6,7 @@ const authenticate = require('../middleware/auth'); // assumes token middleware
 router.post('/create', authenticate, bookingController.createBooking);
 router.post('/', authenticate, bookingController.getBookings);
 router.get('/ByDateBookings', authenticate, bookingController.ByDateBookings);
+router.get('/MyBookingsForTalent', authenticate, bookingController.MyBookingsForTalent);
 
 router.post('/getBookingDetails', authenticate,bookingController.getBookingDetails);
 router.post('/update-status', authenticate,bookingController.updateBookingStatus);
