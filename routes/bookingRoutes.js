fa190861c345c6345d5baffdc3a57cc4f2647346
sherazaleem <107539547+sherazaleem@users.noreply.github.com>
2025-09-09 10:35:42 +0500

    update designation add and get by relation

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -11,7 +11,7 @@ router.get('/MyBookingsForTalent', authenticate, bookingController.MyBookingsFor
 router.post('/getBookingDetails', authenticate,bookingController.getBookingDetails);
 router.post('/update-status', authenticate,bookingController.updateBookingStatus);
 router.get('/bookings-Slots-have-talent', authenticate,bookingController.MyBookingSlotsForTalent);
-router.get('/bookings-Slots-have-client', authenticate,bookingController.MyBookingSlotsForClient);
+router.post('/MyBookingSlotsForClient', authenticate,bookingController.MyBookingSlotsForClient);
 router.post('/bookingsSlotshaveclient', authenticate,bookingController.bookingsSlotshaveclient);
 
 
