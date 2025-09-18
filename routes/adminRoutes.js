3faaef16df125de16e8cbf9cf52769fef3e8f7c8
sherazaleem <107539547+sherazaleem@users.noreply.github.com>
2025-09-18 23:05:14 +0500

    update deal controller

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,9 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
+const talentController = require('../controllers/talentController');
+const authController = require('../controllers/authController');
 const authMiddleware = require('../middleware/auth');
 
 router.post('/register', adminController.register); // Requires authentication (optional)
 router.post('/login', adminController.login);
+router.post('/talent', talentController.getTalents);
+router.post('/client', authController.getAllClients);
 
 module.exports = router;
