07adaf729bb05b71d9093dfb4ce3f5616705c526
sherazaleem <107539547+sherazaleem@users.noreply.github.com>
2025-09-23 22:04:23 +0500

    update counts of employees and attdance and img issue on brand and profile pichture

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 const talentController = require('../controllers/talentController');
 const authController = require('../controllers/authController');
+const feedController = require('../controllers/feedController');
 const authMiddleware = require('../middleware/auth');
 
 router.post('/register', adminController.register); // Requires authentication (optional)
@@ -11,5 +12,6 @@ router.post('/login', adminController.login);
 router.post('/talent', talentController.getTalents);
 router.post('/client', authController.getAllClients);
 router.post('/detailsUser', authController.detailsUser);
+router.post('/feeds', feedController.AdminFeed);
 
 module.exports = router;
