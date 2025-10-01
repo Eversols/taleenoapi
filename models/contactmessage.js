89f6c42561d417bac2c5bc94b8ba9e7c0cf036ef
sherazaleem <107539547+sherazaleem@users.noreply.github.com>
2025-10-02 00:04:13 +0500

    update convosoftserver attandance late check

--- models/contactmessage.js
+++ models/contactmessage.js
@@ -3,6 +3,10 @@ module.exports = (sequelize, DataTypes) => {
   const ContactMessage = sequelize.define(
     'ContactMessage',
     {
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true, // set to false if required
+      },
       name: DataTypes.STRING,
       email: DataTypes.STRING,
       subject: DataTypes.STRING,
